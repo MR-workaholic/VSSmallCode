e87b68dd5f64c37c29b842e90e6c80558821d2d1
CZH <13580518842@163.com>
2018-01-18 09:43:57 +0800

    从前序中序递归重构二叉树

---
+++ J06_ConstructBinaryTree.cpp
@@ -0,0 +1,75 @@
+/*
+1 2 4 7 3 5 6 8
+4 7 2 1 5 3 8 6
+ */
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+TreeNode* reConstructBinaryTree(vector<int> pre, vector<int> vin){
+    if (pre.size() == 0)
+    {
+        return NULL;
+    }
+    if (pre.size() == 1)
+    {
+        return new TreeNode(pre.at(0));
+    }
+    TreeNode* result = new TreeNode(pre.at(0));
+    auto iFind = find(vin.begin(), vin.end(), pre.at(0));
+    vector<int> leftVin(vin.begin(), iFind);
+    vector<int> rightVin(iFind + 1, vin.end());
+
+    auto index = pre.begin();
+    index = index + 1;
+    vector<int> leftPre(index, index + leftVin.size());
+    vector<int> rightPre(index + leftVin.size(), pre.end());
+
+    result->left = reConstructBinaryTree(leftPre, leftVin);
+    result->right = reConstructBinaryTree(rightPre, rightVin);
+
+    return result;
+}
+
+int main(int argc, char **argv){
+    streambuf* backup;
+    ifstream fin;
+    fin.open("data.in");
+    backup = cin.rdbuf();
+    cin.rdbuf(fin.rdbuf());
+
+    string inputline;
+    istringstream iss;
+    while (true)
+    {
+        vector<int> preorder;
+        vector<int>  inorder;
+        getline(cin, inputline);
+        iss = istringstream(inputline);
+        int temp;
+        while (iss >> temp)
+        {
+            preorder.push_back(temp);
+        }
+        getline(cin, inputline);
+        iss = istringstream(inputline);
+        while (iss >> temp)
+        {
+            inorder.push_back(temp);
+        }
+        reConstructBinaryTree(preorder, inorder);
+
+    }
+}
