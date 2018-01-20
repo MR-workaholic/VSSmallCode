2354f030cf143cb97dc7f424f3cf72dcaee71ff7
CZH <13580518842@163.com>
2018-01-20 11:00:55 +0800

    判断序列是否是二叉搜索树的后序遍历序列,完整版

--- J24_SquenceOfBST.cpp
+++ J24_SquenceOfBST.cpp
@@ -1,3 +1,7 @@
+/*
+5 7 6 9 11 10 8
+7 4 6 5
+ */
 #include <iostream>
 #include <fstream>
 #include <string>
@@ -9,9 +13,9 @@ using namespace std;
 
 bool reConstructTree(vector<int> post, vector<int> vin)
 {
-    if ()
+    if (post.size() == 0)
     {
-
+        return true;
     }
     auto tailNode = post.end();
     tailNode--;
@@ -21,7 +25,12 @@ bool reConstructTree(vector<int> post, vector<int> vin)
         return false;
     }
     vector<int> rightChildVin(rootNode + 1, vin.end());
-    vector<int> leftChindVin(vin.begin(), rootNode);
+    vector<int> leftChildVin(vin.begin(), rootNode);
+
+    vector<int> rightChildPost(tailNode - rightChildVin.size(), tailNode);
+    vector<int> leftChildPost(post.begin(), post.begin() + leftChildVin.size());
+
+    return (reConstructTree(leftChildPost, leftChildVin) && reConstructTree(rightChildPost, rightChildVin));
 
 }
 
@@ -56,6 +65,6 @@ int main(int argc, char **argv){
         {
             record.push_back(temp);
         }
-
+        cout << VerifySquenceOfBST(record) << endl;
     }
 }
